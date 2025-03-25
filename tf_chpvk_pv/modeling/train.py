11107476ae63118f9e9228f80e78e606379d032e
GarzonDiegoFEUP <up202010843@up.pt>
2025-03-25 16:39:47 +0000

    fit with different weigths for S and Se.

--- tf_chpvk_pv/modeling/train.py
+++ tf_chpvk_pv/modeling/train.py
@@ -114,6 +114,57 @@ def train_tree_sis_features(
     t_sisso_expression = t_sisso_expression.replace("^","**")
     print('Identified expression for t_sisso: %s' % t_sisso_expression)
     return t_sisso_expression
+
+def train_tree_sis_features_Ch(
+    features_path: Path = INTERIM_DATA_DIR / "features_sisso.csv",
+    train_data_path: Path = PROCESSED_DATA_DIR / "chpvk_train_dataset.csv",
+    test_data_path: Path = PROCESSED_DATA_DIR / "chpvk_test_dataset.csv",
+):
+    # ---- REPLACE THIS WITH YOUR OWN CODE ----
+    logger.info("Training tree model with SISSO features...")
+    # -----------------------------------------
+
+    #train classification trees for the selected descriptors
+    train_df = pd.read_csv(train_data_path, index_col=0)
+    test_df = pd.read_csv(test_data_path, index_col=0)
+    feature_df = pd.read_csv(features_path, index_col=0)
+    
+    sample_weights = train_df['rX'].to_numpy()
+    sample_weights = np.where(sample_weights == 198.4, 2, sample_weights)
+    sample_weights = np.where(sample_weights == 184.40, 2, sample_weights)
+    sample_weights = np.where(sample_weights != 2, 1, sample_weights)
+
+    #depth of the classification tree - user has to choose
+    def rank_tree(labels, feature_space, depth, sample_weights):                       #rank features according to the classification-tree accuracy                             
+        score = []
+        for i in list(range(0,feature_space.shape[1])):  
+            #print(feature_space.columns[i])              # 'i' is a column and 'for' is from the first column to the last one
+            x=np.array(feature_space)[:,i]
+            if (x > 1e6).any():
+                print('Feature %s has values greater than 1e6. Skipping.' % feature_space.columns.values[i])
+                continue                           # take the first column values
+            else:
+                clf = tree.DecisionTreeClassifier(max_depth=depth, class_weight='balanced', criterion='entropy')
+                
+                clf_cv = cross_validate(clf, x.reshape(-1,1), labels, scoring='accuracy', params={'sample_weight': sample_weights})
+                clf_cv_score = np.mean(clf_cv['test_score'])
+                
+                #clf = clf.fit(x.reshape(-1,1), labels)                     # Build a decision-tree classifier from the training set (X, y). X is the values of features (for each for iteration on column) and Y is the target value, here exp_label
+                score.append([feature_space.columns.values[i],clf_cv_score])      # make a list of the feature and the mean accuracy of the all values of that feaure (for different materials)
+        score_sorted=sorted(score,reverse=True,key=lambda x: x[1])     # sort the features based on the accuracy
+        return score_sorted
+
+    labels_train=train_df["exp_label"].to_numpy()
+    labels_test=test_df["exp_label"].to_numpy()
+    rank_list=rank_tree(labels_train, feature_df, 1, sample_weights=sample_weights)
+    tree_pd =pd.DataFrame(rank_list, columns=['feature','tree accuracy'])       # make a new data frame of the feature and the accuracies
+
+    # the first ranked feature is the t_sisso
+    t_sisso_expression=str(rank_list[0][0])   
+    t_sisso_expression = t_sisso_expression.replace("ln","log")
+    t_sisso_expression = t_sisso_expression.replace("^","**")
+    print('Identified expression for t_sisso: %s' % t_sisso_expression)
+    return t_sisso_expression
     
 def evaluate_t_sisso(t_sisso_expression, idx=-1,
                      train_df_path: Path = PROCESSED_DATA_DIR / "chpvk_train_dataset.csv",
@@ -216,6 +267,100 @@ def test_tolerance_factor(t, train_df, test_df, tolerance_factor_dict, df_acc=pd
     tree.plot_tree(clf1_model)
 
     name_figure = 'tree_' + t + '.png'
+    
+    plt.savefig(TREES_DIR / name_figure)
+    
+    acc_train = clf1_model.score(x_train_.reshape(-1,1),labels_train)
+    acc_test = metrics.accuracy_score(labels_test, labels_pred_)
+    
+    #General accuracy
+    all_data = np.append(x_train_, x_test_)
+    all_labels = np.append(labels_train, labels_test)
+    labels_all_data = clf1_model.predict(all_data.reshape(-1,1))
+    acc_all = metrics.accuracy_score(all_labels, labels_all_data)
+    
+    print('Classification tree accuracy (for ' + t + ') on the train set: %f.' % acc_train)
+    print('Classification tree accuracy (for ' + t + ') on the train set (5 fold CV): %f.' % clf1_cv_score)
+    print('Classification tree accuracy (for ' + t + ') on the test set: %f.' % acc_test)
+
+    if n_tresh == 2:
+        threshold_=[clf1_model.tree_.threshold[0],clf1_model.tree_.threshold[4]]
+
+        # add t threshold to dictionary
+        tolerance_factor_dict[t].append(threshold_)
+
+        print('%f < ' % tolerance_factor_dict[t][1][0] +  t +' < %f indicates stable perovskites.' % tolerance_factor_dict[t][1][1])
+    elif n_tresh == 1:
+        threshold_= clf1_model.tree_.threshold[0]
+
+        #Add threshold to the dictionary
+        tolerance_factor_dict[t].append(threshold_)
+
+        print(t + ' < %f indicates stable perovskites.' % tolerance_factor_dict[t][1])
+              
+    df_acc.loc['train_data', t] = acc_train
+    df_acc.loc['test_data', t] = acc_test
+    df_acc.loc['all_data', t] = acc_all
+    
+    #get accuracy per X anion with the rX
+    dict_ch = {133.15:'F',
+               180.61:'Cl',
+               198.14:'Se',
+               195.56:'Br',
+               184.40:'S',
+               219.49:'I'
+              }       
+    
+    for rx in train_df.rX.unique():
+        x_test_ch = test_df.loc[test_df.rX == rx, t].to_numpy()
+        x_train_ch = train_df.loc[train_df.rX == rx, t].to_numpy()
+        
+        labels_test_ch = labels_test[test_df.rX == rx]
+        labels_train_ch = labels_train[train_df.rX == rx]
+        
+        labels_pred_ch=clf1_model.predict(x_test_ch.reshape(-1,1))
+        
+        acc_train_ch = clf1_model.score(x_train_ch.reshape(-1,1),labels_train_ch)
+        acc_test_ch = metrics.accuracy_score(labels_test_ch, labels_pred_ch)
+        
+        df_acc.loc['train_data_' + dict_ch[rx], t] = acc_train_ch
+        df_acc.loc['test_data_' + dict_ch[rx], t] = acc_test_ch 
+        
+    df_acc.loc['5-fold CV', t] = clf1_cv_score
+    
+    return df_acc, clf1_model
+
+def test_tolerance_factor_Ch(t, train_df, test_df, tolerance_factor_dict, df_acc=pd.DataFrame(),
+                          n_tresh=1, cl_w = 'balanced', crit='entropy'):
+    
+    
+    
+    labels_train=train_df["exp_label"].to_numpy()
+    labels_test=test_df["exp_label"].to_numpy()
+
+    sample_weights = train_df['rX'].to_numpy()
+    sample_weights = np.where(sample_weights == 198.4, 2, sample_weights)
+    sample_weights = np.where(sample_weights == 184.40, 2, sample_weights)
+    sample_weights = np.where(sample_weights != 2, 1, sample_weights)
+    
+    if len(df_acc.columns) == 0:
+        df_acc = pd.DataFrame(columns=['t_sisso', 't', 'tau', 't_jess'], index=['train_data', 'test_data', 'all_data'] )
+        
+    x_train_=train_df[t].to_numpy()
+    x_test_=test_df[t].to_numpy()
+    
+    
+    clf1_tree = tree.DecisionTreeClassifier(max_depth=n_tresh, class_weight=cl_w, criterion=crit)
+    
+    clf1_cv = cross_validate(clf1_tree, x_train_.reshape(-1,1), labels_train, scoring='accuracy')
+    
+    clf1_cv_score = np.mean(clf1_cv['test_score'])
+    
+    clf1_model = clf1_tree.fit(x_train_.reshape(-1,1),labels_train, sample_weight=sample_weights)
+    labels_pred_=clf1_model.predict(x_test_.reshape(-1,1))
+    tree.plot_tree(clf1_model)
+
+    name_figure = 'Ch_tree_' + t + '.png'
     
     plt.savefig(TREES_DIR / name_figure)
     
